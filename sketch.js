afb3bd8618ce6994d97ad351886904b87a00b8a9
Karan Foster <kfoster@wlapcs.org>
2017-03-29 01:14:56 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -1,16 +1,21 @@
 var a = 50;
 var b = 50;
+var newWidth = 50;
 var newHeight = a + b;
 
 //setup function executes once
 function setup(){
     createCanvas(600,400);
-    //background(50);
+    background(50);
 }
 
 //draw function loops forever
 function draw(){
-    background(50);
-    fill(150,0,255);
-   ellipse(mouseX,mouseY,newWdth,newHeight); 
+    //background(50);
+    if(mouseIsPressed){
+        noStroke();
+        fill(150,0,255,255,100);
+   rect(mouseX,mouseY,newWidth,newHeight); 
+        
+    }
 }
